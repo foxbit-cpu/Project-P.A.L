f32853529bfece07b6cb8f9aa856c8849c811169
foxbit-cpu <korsakin.zhenya@mail.ru>
2026-02-03 13:43:39 +1100

    Add files via upload

---
+++ code_aid/snippets/init.py
@@ -0,0 +1,9 @@
+# snippets/__init__.py
+# Этот файл может быть пустым или содержать только эти строки:
+
+from .python_snippets import python_snippets
+from .java_snippets import java_snippets
+from .cpp_snippets import cpp_snippets
+from .csharp_snippets import csharp_snippets
+
+__all__ = ['python_snippets', 'java_snippets', 'cpp_snippets', 'csharp_snippets']
---
+++ code_aid/utils/init.py
@@ -0,0 +1,4 @@
+from .helpers import *
+
+__all__ = ['load_json_file', 'save_json_file', 'format_code_time_complexity', 
+           'get_timestamp', 'count_snippets_by_language', 'filter_snippets_by_tag']
